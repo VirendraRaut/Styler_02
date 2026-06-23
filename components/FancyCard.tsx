7ce2bba9037e7c53c7e24a970a7a48ae90fded1e
VirendraRaut <virendraraut1306@gmail.com>
2026-06-23 12:33:06 +0530

    rnfs

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function FancyCard() {
   return (
     <View>
       <Text>FancyCard</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
