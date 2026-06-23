2ea557c8ffd2c024b4349756078d7e2cf3b0e16b
VirendraRaut <virendraraut1306@gmail.com>
2026-06-23 13:11:17 +0530

    commit

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,12 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 export default function FancyCard() {
   return (
     <View>
-      <Text>Trending Places</Text>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.elevatedCard]}>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1000',
+          }}
+          style={[styles.cardImage]}
+        />
+      </View>
+      <View style={styles.cardBody}>
+        <Text style={styles.cardTitle}>Lake</Text>
+        <Text style={styles.cardLabel}>Germany</Text>
+        <Text style={styles.cardDescription}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum,
+          culpa.
+        </Text>
+        <Text style={styles.cardFooter}>CardFooter</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+  card: {},
+  elevatedCard: {},
+  cardImage: {
+    height: 150,
+  },
+  cardBody: {},
+  cardTitle: { color: 'white' },
+  cardLabel: { color: 'white' },
+  cardDescription: { color: 'white' },
+  cardFooter: { color: 'white' },
+});
